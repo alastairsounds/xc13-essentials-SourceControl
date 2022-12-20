e66abb98f2ea1925d8ff396223da3418c98fe396
alastairsounds <alastairscheuermann@gmail.com>
2022-12-20 09:01:32 -0700

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Alastair Scheuermann on 2022-12-20.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
